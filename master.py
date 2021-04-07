8f5e1707eeb9124e64a398063fb135708391194b
guru2001 <guru.ravi22x@gmail.com>
2021-04-07 10:38:11 +0530

    Multi Threading complete

--- master.py
+++ master.py
@@ -2,10 +2,43 @@ import Pyro5.api
 from sys import stdin
 import networkx as nx 
 import matplotlib.pyplot as plt 
+import multiprocessing
+import threading 
+
+lock = threading.Lock()
 
 G = nx.DiGraph() 
 urls_done = set()
 adj_graph = {}
+temp = []
+
+def scraping_function(i, urls):
+    ns = Pyro5.api.locate_ns()
+    server_list = ns.list()
+    worker_uris = []
+
+    for server in server_list:
+        if server != 'Pyro.NameServer':
+            worker_uris.append(server_list[server])
+    workers = [Pyro5.client.Proxy(uri) for uri in worker_uris]
+
+    child_urls, error_urls = workers[i].scrape(urls)
+    lock.acquire()
+    
+    for url in child_urls:
+        urls_done.add(url)
+    for url in child_urls:
+        adj_graph[url] = set()
+        for i in child_urls[url]:
+            if i not in adj_graph[url]:
+                adj_graph[url].add(i)
+
+    for a in child_urls:
+        temp.extend(child_urls[a])
+
+    lock.release()
+
+
 def process(line):
     # get worker nodes
     ns = Pyro5.api.locate_ns()
@@ -15,6 +48,7 @@ def process(line):
         if server != 'Pyro.NameServer':
             worker_uris.append(server_list[server])
     workers = [Pyro5.client.Proxy(uri) for uri in worker_uris]
+    # process_pool = multiprocessing.Pool(len(workers))
     # parse and process line
     ls = line.split()
     if ls[0] == 'seed':
@@ -30,25 +64,22 @@ def process(line):
                 if url in urls_done:
                     urls.remove(url)
                     continue
+
             dist = len(urls)//len(workers)
-            
+            print(dist, len(urls))
             if dist > 0:
                 temp = []
+                threads = []
                 for i in range(len(workers)):
-                    child_urls, error_urls = workers[i].scrape(urls[i * dist: min(dist * (i + 1), len(urls))]) #remember input is list of urls not url
-                    for url in child_urls:
-                        urls_done.add(url)
-                    for url in child_urls:
-                        adj_graph[url] = set()
-                        for i in child_urls[url]:
-                            if i not in adj_graph[url]:
-                                adj_graph[url].add(i)
+                    t = threading.Thread(target=scraping_function, args=(i, urls[i * dist: min(dist * (i + 1), len(urls))]))
+                    threads.append(t)
+                    t.start()
 
-                    for a in child_urls:
-                        temp.extend(child_urls[a])
-
+                for i in range(1,len(threads)):
+                    threads[i].join()
 
             else:
+                print("dsadsadasd")
                 child_urls, error_urls = workers[0].scrape(urls)
                 for url in urls:
                     urls_done.add(url)
@@ -60,6 +91,7 @@ def process(line):
                 temp = []
                 for a in child_urls:
                     temp.extend(child_urls[a])
+                print(temp)
             urls = temp
 
 
@@ -79,9 +111,9 @@ def process(line):
             # urls = temp
 
     if ls[0] == 'print':
-        # nx.draw(G,with_labels=True) 
-        # plt.show()
-        # print(adj_graph)
+        nx.draw(G,with_labels=True) 
+        plt.show()
+        print(adj_graph)
         if ls[1] in adj_graph:
             print(adj_graph[ls[1]])
         else:
@@ -102,5 +134,5 @@ if __name__ == '__main__':
         if line == '' or line == 'quit': # If empty string is read then stop the loop
             break
         process(line) # perform some operation(s) on given string
-    
-   
+
+
