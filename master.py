368055b406dc2da119d634849cbd7070844612d2
Adarsh Dharmadevan <adarshdharmadevan@gmail.com>
2021-04-29 18:08:30 +0530

    have to be debugged

--- master.py
+++ master.py
@@ -1,60 +1,31 @@
 import Pyro5.api 
 from sys import stdin
 from pyvis.network import Network
-import threading
 
 
-MIN_URL_LIMIT = 20
-
-
-lock = threading.Lock()
-scraped_urls = set()
-adjacency_list = {}
-worker_uris = []
-next_level_urls = set()
-
-def scrape(worker_uri,urls):
-    print("scraping")
-    print(urls)
-    child_dict = {}
-    try:
-        worker = Pyro5.client.Proxy(worker_uri)
-        child_dict, error_urls = worker.scrape(urls)
-    except:
-        print("".join(Pyro5.errors.get_pyro_traceback()))
-        error_urls = urls
-        if worker_uri in worker_uris:
-            worker_uris.remove(worker_uri)
-    lock.acquire()
-    # print("child_dict = {}".format(child_dict))
-    # print("error_urls = {}".format(child_dict))
-    for parent_url in child_dict.keys():
-        child_urls = child_dict[parent_url]
-        scraped_urls.add(parent_url)
-        if child_urls:
-            adjacency_list[parent_url] = child_urls
-            next_level_urls.update(child_urls)
-    # print("next_level_urls = {}".format(next_level_urls))
-    lock.release()
-
-def process(line):
-
+    
+def get_workers():
     # get worker nodes
     ns = Pyro5.api.locate_ns()
     server_list = ns.list()
-    worker_uris = []
+    del server_list['Pyro.NameServer']
+    print(server_list)
+    workers = []
     for server in server_list:
-        if server != 'Pyro.NameServer':
             try:
                 worker = Pyro5.client.Proxy(server_list[server])
                 if worker.test():
-                    worker_uris.append(server_list[server])
+                    workers.append(worker)
             except:
                 pass
-    if not worker_uris:
+    if not workers:
         print("No workers detected")
-        return
+    return workers
     # print(worker_uris)
+
+def process(line):
+
+    
 
     ls = line.split()
 
@@ -64,79 +35,45 @@ def process(line):
             print("Usage: seed seed_url depth_pages_to_be_scraped")
             return
 
-        scrape_depth = int(ls[2])
-        urls_to_be_scraped = {ls[1]}
-        next_level_urls.clear()
-
-        for l in range(scrape_depth):
-
-            # print("urls_to_be_scraped = {}".format(urls_to_be_scraped))
-            # remove already scraped urls and add their children
-            urls_to_be_scraped_list = list(urls_to_be_scraped)
-            for url in urls_to_be_scraped_list:
-                if url in scraped_urls:
-                    urls_to_be_scraped.remove(url)
-                    next_level_urls.update(adjacency_list[url])
-
-            
-            # scrape the urls using the workers
-            num_urls_per_worker = max(
-                (len(urls_to_be_scraped)//len(worker_uris))+1,
-                MIN_URL_LIMIT)
-            worker_index = 0
-            threads = {}
-            while urls_to_be_scraped:
-                urls = []
-                for i in range(min(len(urls_to_be_scraped),
-                               num_urls_per_worker)):
-                    urls.append(urls_to_be_scraped.pop())
-                threads[worker_index] = threading.Thread(
-                    target=scrape, args=(worker_uris[worker_index],urls))
-                threads[worker_index].start()
-                worker_index += 1
-            for i in range(worker_index):
-                threads[i].join()
-
-            # have to write code to handle error urls
-
-            # loading next level of urls to be scraped
-            urls_to_be_scraped = next_level_urls.copy()
+        workers = get_workers()
+        workers[0].crawl([ls[1]], ls[2])
         
         print("DONE")
         return
 
-    if ls[0] == 'graph':
-        net = Network()
-        net.add_nodes(scraped_urls)
-        for x in adjacency_list.keys():
-            for y in adjacency_list[x]:
-                net.add_node(y) #since error urls are not handled rn
-                net.add_edge(x,y)
-        # net.enable_physics(False)
-        if len(ls)>1:
-            if ls[1] == "-s":
-                net.show_buttons()
-            else:
-                print("Command not supported")
-                return
-        net.show("graph.html")
-        print("DONE")        
-        return
+    # if ls[0] == 'graph':
+    #     net = Network()
+    #     net.add_nodes(scraped_urls)
+    #     for x in adjacency_list.keys():
+    #         for y in adjacency_list[x]:
+    #             net.add_node(y) #since error urls are not handled rn
+    #             net.add_edge(x,y)
+    #     # net.enable_physics(False)
+    #     if len(ls)>1:
+    #         if ls[1] == "-s":
+    #             net.show_buttons()
+    #         else:
+    #             print("Command not supported")
+    #             return
+    #     net.show("graph.html")
+    #     print("DONE")        
+    #     return
 
-    if ls[0] == 'update':
-        url = ls[1]
-        if url not in scraped_urls:
-            print("Given url has not been scraped yet")
-            print("please use seed command instead")
-            return
-        worker = Pyro5.client.Proxy(worker_uris[0])
-        child_urls, error_urls = worker.scrape([url])
-        adjacency_list[url].update(child_urls[url])
-        print("DONE")
-        return
+    # if ls[0] == 'update':
+    #     url = ls[1]
+    #     if url not in scraped_urls:
+    #         print("Given url has not been scraped yet")
+    #         print("please use seed command instead")
+    #         return
+    #     worker = Pyro5.client.Proxy(worker_uris[0])
+    #     child_urls, error_urls = worker.scrape([url])
+    #     adjacency_list[url].update(child_urls[url])
+    #     print("DONE")
+    #     return
 
     if ls[0] == 'print':
-        print(adjacency_list)
+        workers = get_workers()
+        print(workers[0].get([ls[1]], ls[2]))
 
     else:
         print("Command not supported")
--- worker.py
+++ worker.py
@@ -1,5 +1,31 @@
 import Pyro5.api
+import multiprocessing as mp
+import threading
 from requests_html import HTMLSession
+
+MIN_URL_LIMIT = 20
+
+worker_number = -1
+
+def get_other_workers():
+    # get worker nodes
+    ns = Pyro5.api.locate_ns()
+    server_list = ns.list()
+    workers = []
+    selfname = "Worker_" + str(worker_number)
+    del server_list['Pyro.NameServer']
+    for server in server_list:
+        if server != selfname:
+            try:
+                worker = Pyro5.client.Proxy(server_list[server])
+                if worker.test():
+                    workers.append(worker)
+            except:
+                pass
+    if not workers:
+        print("No other workers detected")
+    return workers
+    # print(workers)
 
 
 @Pyro5.api.expose
@@ -7,39 +33,119 @@ from requests_html import HTMLSession
 class Worker:
     def __init__(self):
         self.pages = {}
-        self.session = HTMLSession()
+        self.adjacency_list = {}
+        self.worker_incharge = {}
+        self._lock = threading.Lock()
+    
+    def test(self):
+        return True
 
     def scrape(self,url_list):
+        session = HTMLSession()
         child_urls = {}
         error_urls = []
         for url in url_list:
             try:
-                r = self.session.get(url)
+                r = session.get(url)
                 withoutrender = r.html.absolute_links
                 # r.html.render()
                 withrender = r.html.absolute_links
                 child_urls[url] = withoutrender.union(withrender)
-                self.pages[url] = r.html
+                with self._lock:
+                    self.pages[url] = r.html
             except:
                 error_urls.append(url)
-        # print("child URLs")
-        # print(child_urls)
-        # print("error URLs")
-        # print(error_urls)
+        print("child URLs")
+        print(child_urls)
+        print("error URLs")
+        print(error_urls)
         return child_urls, error_urls
 
-    def get_pages(self,urls):
-        return { url: self.pages[url] for url in urls }
+    def get_pages(self,url):
+        return self.pages[url]
+        # return { url: self.pages[url] for url in urls }
 
-    def test(self):
-        return True
+    def remove_duplicates(self,urls):
+        # with self._lock:
+        # already_processed_urls = list(self.adjacency_list.keys())
+        print(urls)
+        new_urls = []
+        for url in urls:
+            if url not in self.adjacency_list.keys():
+                new_urls.append(url)
+        return new_urls
 
+    @Pyro5.server.oneway
+    def crawl(self,urls, depth):
+        print("crawling depth {}".format(depth))
+        # remove already scraped urls
+        urls = self.remove_duplicates(urls)
+        # scrape
+        cu, eu = self.scrape(urls)
+        # update adjacency list
+        with self._lock:
+            self.adjacency_list.update(cu)
+        print('al')
+        print(self.adjacency_list)
+        # remove already scraped urls from child urls
+        urls_to_be_crawled = self.remove_duplicates(list(cu.values()))
+        print('child urls to be crawled')
+        print(urls_to_be_crawled)
+        # return if crawl depth is satisfied
+        if depth <= 1:
+            return
+        # distribute child urls to other workers
+        workers = get_other_workers()
+        print("other workers")
+        print(workers)
+        num_urls_per_worker = max(
+            (len(urls_to_be_crawled)//len(workers))+1,
+            MIN_URL_LIMIT)
+        print('num_urls_per_worker')
+        print(num_urls_per_worker)
+        next_urls = []
+        for i in range(min(len(urls_to_be_crawled),
+                                num_urls_per_worker)):
+                    url = urls_to_be_crawled.pop()
+                    next_urls.append(url)
+        print('next_urls')
+        print(next_urls)        
+        worker_index = 0    # need a better way to id workers
+        while urls_to_be_crawled:
+            urlgroup = []
+            with self._lock:
+                for i in range(min(len(urls_to_be_crawled),
+                                num_urls_per_worker)):
+                    url = urls_to_be_crawled.pop()
+                    urlgroup.append(url)
+                    self.worker_incharge[url] = worker_index
+            workers[worker_index].crawl(urlgroup,depth-1)
+            worker_index += 1
+        print('finish depth {}".format(depth)')
+        self.crawl(next_urls,depth-1)
+        
+    def get(self, urls, depth):
+        al = {}
+        for url in urls:
+            if url in self.adjacency_list:
+                al[url] = self.adjacency_list[url]
+            else:
+                pass
+        return al
+        
+# @Pyro5.api.expose
+# @Pyro5.api.behavior(instance_mode="single")
+# class Server:
+#     def __init__(self):
+#         self.job_queue = mp.Queue()
+#         self.worker = Worker()
 
-# testing
-# w = Worker()
-# print(w.scrape(["https://open.spotify.com/"]))
-
+#     def test(self):
+#         return True
    
+#     def seed(self,urls, depth):
+#         self.job_queue.put(('CRAWL',urls,depth))
+#         return True
 
 daemon = Pyro5.server.Daemon()         
 ns = Pyro5.api.locate_ns()     
